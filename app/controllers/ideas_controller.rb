d56e05950c2544cb6e733c71888e951b6294275b
Edgar Duran <edgarduran@Edgars-MacBook-Pro-8.local>
2016-03-21 17:51:37 -0600

    db created, idea model, view, form

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,2 +1,10 @@
 class IdeasController < ApplicationController
+
+  def index
+
+  end
+
+  def create
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
-  
+  root to: "ideas#index"
+  resources :ideas, only: [:index, :create]
 
 end
