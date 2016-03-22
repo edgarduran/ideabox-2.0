bf658a194c8be86b7b0a2f20b6c2894c305028c8
Edgar Duran <edgarduran@Edgars-MacBook-Pro-8.local>
2016-03-21 19:25:07 -0600

    create idea

---
+++ app/assets/javascripts/ideas.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+  function renderPost(post) {
+    $('#latest-posts').append(
+      "<div class='post' data-id='" + post.id + "'><h6> Published on: " +
+      post.created_at + "</h6><p>" + post.description
+      + "</p>" +
+      "<button id='delete-post' name=button-fetch class='btn btn-default'>Delete</button>"
+       + "</div>"
+    )
+  }
+
+  $.ajax({
+    type: "GET",
+    url : "https://turing-birdie.herokuapp.com/api/v1/posts.json",
+    success: function(posts) {
+      $.each(posts, function(index, post){
+        renderPost(post)
+      })
+    }
+  })
+
+  $('#create-post').on('click', function(){
+    var postParams = {
+      post: {
+        description: $('#post-description').val()
+      }
+    }
+    $.ajax({
+      type: 'POST',
+      url : 'https://turing-birdie.herokuapp.com/api/v1/posts.json',
+      data: postParams,
+      success: function(newPost) {
+        renderPost(newPost)
+      },
+      error: function(xhr) {
+        console.log(xhr.responseText)
+      }
+    })
+  })
+
+  $('#latest-posts').delegate('#delete-post', 'click', function(){
+    var $post = $(this).closest('.post')
+
+    $.ajax({
+      type: 'DELETE',
+      url : "https://turing-birdie.herokuapp.com/api/v1/posts/" +
+            $post.attr('data-id')+ ".json",
+      success: function(){
+        $post.remove()
+      },
+      error: function(xhr) {
+        console.log(xhr.responseText)
+      }
+    })
+  })
+
+
+
+})
