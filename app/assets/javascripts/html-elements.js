6acc55261767544e1953223556201afb5e8c4b4d
Edgar Duran <edgar.eduran@gmail.com>
2016-04-10 21:43:00 -0600

    extracted html elements to another file

---
+++ app/assets/javascripts/html-elements.js
@@ -0,0 +1,52 @@
+function editForm() {
+  return $(
+    "<div class='container'>"
+    +"<div class='row'>"
+    +"<form class='col s12'>"
+    +"<h5>Make changes here</h5>"
+    +"<div class='row'>"
+    +"<div class='input-field col s6'>"
+    +"<i class='material-icons prefix'>star_rate</i>"
+    +"<label for='icon_prefix'>Title</label>"
+    +"<input class='form-control' id='edit-title' type='text' class='validate'>"
+    +"</div>"
+    +"</div>"
+    +"<div class='row'>"
+    +"<div class='input-field col s6'>"
+    +"<i class='material-icons prefix'>comment</i>"
+    +"<label for='star_rate'>Body</label>"
+    +"<textarea id='edit-body' class='materialize-textarea' length='120'></textarea>"
+    +"<a class='btn-floating btn-large waves-effect waves-light green' id='save-changes' value='Save Changes'><i class='material-icons'>done</i></a>"
+    +"</div>"
+    +"</div>"
+    +"</form>"
+    +"</div>"
+    +"</div>"
+  )
+};
+
+
+function individualIdea(idea) {
+  return $(
+    "<div class='idea' idea-id='" + idea.id + "'>"
+    +"<div class='row'>"
+    +"<div class='col s12 m5'>"
+    +"<div class='card blue-grey darken-1'>"
+    +"<div class='card-content white-text'>"
+    +"<h1 id='idea-title'>" + idea.title + "</h1>"
+    +"<p id='idea-body'>" + idea.body + "</p>"
+    +"<h5>Idea Qaulity:</h5>"
+    +"<h4 id='quality'>" + idea.quality + "</h4>"
+    +"</div>"
+    +"<div class='card-action'>"
+    +"<button id='thumbs-up'>Thumbs Up</button>"
+    +"<button id='thumbs-down'>Thumbs Down</button><br><br>"
+    +"<button id='edit-idea'>Edit Idea</button>"
+    +"<button id='delete-idea'>Delete</button>"
+    +"</div>"
+    +"</div>"
+    +"</div>"
+    +"</div>"
+    +"</div>"
+  )
+};
